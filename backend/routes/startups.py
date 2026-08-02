03b2e1ca11ea0009a365decaab9125878c9fa658
sarbesh07 <paudelsarbesh24@gmail.com>
2026-08-02 07:58:32 +0545

    updated startups.py

--- backend/routes/startups.py
+++ backend/routes/startups.py
@@ -34,24 +34,7 @@ def get_startup(startup_id):
         conn.close()
 
 @startups_bp.route("/", methods=["POST"])
-def create_startup():
-    data = request.get_json()
-    conn = get_connection()
-    try:
-        with conn.cursor() as cur:
-            cur.execute("""
-                INSERT INTO startup
-                    (name, registration_no, province, sector,
-                     annual_turnover, registered_on, owner_name, contact_email)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-                RETURNING *
-            """, (
-                data["name"], data["registration_no"], data["province"],
-                data.get("sector"), data.get("annual_turnover"),
-                data["registered_on"], data.get("owner_name"),
-                data.get("contact_email")
-            ))
-            conn.commit()
+..... // delete it later 
             return jsonify(cur.fetchone()), 201
     finally:
         conn.close()
